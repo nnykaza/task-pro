968547ae51199d6a6542f180f262b052289259ca
nnykaza <144247811+nnykaza@users.noreply.github.com>
2023-12-13 12:58:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-document.getElementById('task-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+// app.js
 
-    const taskName = document.getElementById('task-name').value;
-    const taskPriority = document.getElementById('task-priority').value;
-    const taskCategory = document.getElementById('task-category').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const taskForm = document.getElementById('taskForm');
+    const tasksList = document.getElementById('tasksList');
+    const profileDetails = document.getElementById('profileDetails');
 
-    // You can perform further validation here before sending the data to the backend
+    taskForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const title = document.getElementById('taskTitle').value;
+        const description = document.getElementById('taskDescription').value;
+        const priority = document.getElementById('taskPriority').value;
+        const status = document.getElementById('taskStatus').value;
 
-    console.log('Task Name:', taskName);
-    console.log('Priority:', taskPriority);
-    console.log('Category:', taskCategory);
+        // Perform AJAX or fetch request to save the task on the server
+        // For simplicity, we'll just add it to the list here
+        const taskItem = document.createElement('li');
+        taskItem.innerHTML = `<strong>${title}</strong> - ${description}, Priority: ${priority}, Status: ${status}`;
+        tasksList.appendChild(taskItem);
 
-    // Reset the form after processing
-    this.reset();
+        // Clear the form fields
+        taskForm.reset();
+    });
+
+    // Example: Fetch user profile details from the server and display them
+    fetch('/api/user/profile')
+        .then(response => response.json())
+        .then(profile => {
+            profileDetails.innerHTML = `<p><strong>Name:</strong> ${profile.name}</p>
+                                       <p><strong>Email:</strong> ${profile.email}</p>`;
+        })
+        .catch(error => console.error('Error fetching user profile:', error));
 });
